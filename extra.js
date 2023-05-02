cb6faab99cbfa86977eca3c30c448227fa70494a
Charles Berghausen <cberghausen@paypal.com>
2023-05-02 17:50:57 -0400

    deleting old docs

--- extra.js
+++
@@ -1,3 +0,0 @@
-var footer = document.getElementsByClassName('md-footer-copyright')[0];
-
-footer.innerHTML = '<p>Maintained with <span id="heart-icon">❤</span> at <a id="paypal-github-link" href="https://paypal.github.io/">PayPal</a></p>'
